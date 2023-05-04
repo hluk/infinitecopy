ad46ed7c378c5e4e7e95751c2a3892dab5b32286
Lukas Holecek <hluk@email.cz>
2023-05-04 15:36:39 +0200

    Tests: Start test function with clean session

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,15 +9,15 @@ from pytest import fixture
 from infinitecopy.__main__ import createApp, createDbPath, initApp, serverName
 from infinitecopy.Client import Client
 
-SESSION = "__TEST__"
+SESSION = "__TEST{}__"
 APP_SESSION = "__TESTAPP__"
-last_session_id = 0
+_last_session_id = 0
 
 
-def wait_for_server():
+def wait_for_server(session):
     client = Client()
     retry = 20
-    server_name = serverName(SESSION)
+    server_name = serverName(session)
     while not client.connect(server_name):
         if retry == 0:
             assert False
@@ -27,9 +27,13 @@ def wait_for_server():
     client.disconnect()
 
 
-@fixture(scope="session")
+@fixture
 def server():
-    initApp(SESSION)
+    global _last_session_id
+    _last_session_id += 1
+    session = SESSION.format(_last_session_id)
+    initApp(session)
+
     path = createDbPath()
     try:
         os.remove(path)
@@ -37,13 +41,13 @@ def server():
         pass
 
     def run_client(*args):
-        app = createApp(args=["--session", SESSION, *args])
+        app = createApp(args=["--session", session, *args])
         assert app is None
 
-    args = [sys.executable, "-m", "infinitecopy", "--session", SESSION]
+    args = [sys.executable, "-m", "infinitecopy", "--session", session]
     with Popen(args) as proc:
         try:
-            wait_for_server()
+            wait_for_server(session)
             yield run_client
         finally:
             proc.terminate()
